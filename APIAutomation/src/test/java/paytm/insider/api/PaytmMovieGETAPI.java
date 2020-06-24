87ba9bdffa7dd3e4a7cac6aaa72f1f01ad517f86
basavaraju.s <basavaraju.s@simplilearn.net>
2020-06-24 16:38:47 +0530

    gives movie release dates

--- APIAutomation/src/test/java/paytm/insider/api/PaytmMovieGETAPI.java
+++ APIAutomation/src/test/java/paytm/insider/api/PaytmMovieGETAPI.java
@@ -1,19 +1,29 @@
 package paytm.insider.api;
 
+import java.awt.List;
 import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import junit.framework.Assert;
 
 public class PaytmMovieGETAPI {
 	
 	@Test
-	void getMovieDetails() throws UnsupportedEncodingException {
+	void getMovieDetails() throws UnsupportedEncodingException, ParseException {
 		//Specify BaseURI
 		RestAssured.baseURI="https://apiproxy.paytm.com/v2/movies/upcoming";
 		
@@ -25,8 +35,44 @@ public class PaytmMovieGETAPI {
 		
 		//Print response in console window
 		String ResponseBody=response.getBody().asString();
+		
+	//	Assert.assertEquals(ResponseBody.contains("K.G.F. Chapter 2"),true);
 		System.out.println("Response Body is : "+ResponseBody);
-		Assert.assertEquals(ResponseBody.contains("K.G.F. Chapter 2"),true);
+		String MovieDates = response.jsonPath().getString("upcomingMovieData.releaseDate").replaceAll("[\\[\\](){}]","");
+		String[] MoviesDates2=MovieDates.split(",");
+		//String [] Titles=MovieTitle.split(",");
+		 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd ");
+		System.out.println("movie Release Dates :"+MovieDates);
+		for(String Date:MoviesDates2) {
+			System.out.println(Date);
+			
+			   LocalDateTime now = LocalDateTime.now();
+			    String TodayDate =dtf.format(now);
+			    LocalDate ld = LocalDate.parse( Date , dtf );
+			    
+			    
+			    
+			    System.out.println("Today Date" +TodayDate);
+			
+		}
+//		ResponseBody responsebody=response.body();
+		
+//		 Object object = response.jsonPath().get("releaseDate");
+//		 System.out.println("Body   :   "   +responsebody.asString());
+		 
+//		 JsonPath jsonPathEvaluator = response.jsonPath();
+//		 Object MovieName = jsonPathEvaluator.get("upcomingMovieData.movieTitle");
+		
+		String MovieTitle=response.jsonPath().getString("upcomingMovieData.movieTitle");
+		String [] Titles=MovieTitle.split(",");
+		
+		 System.out.println("MovieName received from Response " + MovieTitle);
+		for(String Title:Titles) {
+			System.out.println(Title);
+		}
+		  
+	
+		
 		//status code validation
 		int statuscode =response.getStatusCode();
 		System.out.println("Status code is :" +statuscode);
@@ -36,7 +82,7 @@ public class PaytmMovieGETAPI {
 		String statusline= response.getStatusLine();
 		Assert.assertEquals(statusline, "HTTP/1.1 200 OK");
 		System.out.println("Status line is :"+statusline);
-		String Dates= response.jsonPath().getString("movie_name");
+		String Dates= response.jsonPath().getString("upcomingMovieData.movie_name");
 		System.out.println("Movie Names: " +Dates);
 		
 	}
